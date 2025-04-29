dcc00e7a3eb60d2a524ec7ea7601141cda54c7a7
Carboneinerte <hugo.calligaro@gmail.com>
2025-04-29 01:31:00 +0000

    improved analyssis workflow

--- module/misc.py
+++ module/misc.py
@@ -30,8 +30,25 @@ def cell_class():
         }
     return dict_temp
 
-def clock_genes_list():
+def genes_list(type_):
     
-    clock_genes = ['Arntl','Clock','Cry1','Cry2','Nr1d1',"Per1",'Per2','Per3','Rora','Rorb','Rorc',"Npas2"]
+    dict_list = {
+    'clock' : ['Arntl','Clock','Cry1','Cry2','Nr1d1',"Per1",'Per2','Per3','Rora','Rorb','Rorc',"Npas2"],
+
+    'mitochondria' : ["Acacb","Acod1","Agxt2","Aldh1l2","Arg2","Asb9","Bbc3","Coq8a","Cps1","Cyp24a1","Ddit4","Decr1","Dhfr","Diablo",
+                    'Elac2','Etnppl','Fam210b','Fmc1','Gcat','Gldc','Glud1','Gstk1','Hadhb','Hspa9','Idh2',"Isca1","Lrpprc","Mrm3","Mtif2","Mtus1",
+                    "Nadk2","Nfu1",'Nubpl','Ogdh','Phykpl','Pmaip1','Pnkd','Ppp3r2','Ppp6c','Prorp','Rab32','Rsad1','Selenoo','Star','Tdh','Tufm',
+                    "Vwa8","Acaa2","Glyat","Hibadh","Abcb8","Afg",'Bdh1','Chchd3','Cox7a1','Cox8b','Cpt2','Cyp11a1','Cyp11b2','Cyp27a1',
+                    'Dhodh','Immt','Mcu','Mcub','Mcur1','Micu1','Mpc1','Mpc2',"Mtfp1","Ndufa12","Ndufaf6","Ndufs1","Ndufs4",'Ndufv2',"Opa1","Phb2",
+                    "Pisd",'Sdha','Sdhb','Sdhc','Sfxn1','Slc25a20','Slc25a22','Slc25a28','Slc25a31','Slc25a51','Slc8b1',"Smdt1","Timm50","Ucp1",
+                    "Ucp3","Aifm1","Coa7","Cpox","Cyct","Micu2","Htra2","Aldh2",'Sod2','Acss1','Ak4','Aldh4a1','Dglucy','Fdx1','Grpel1','Hadh',
+                    'Hspd1','Maip1','Mmut','Ndufa9','Otc','Pde12','Pdk1','Pdk4','Pitrm1','Ppif','Prodh','Sardh','Sirt4','Twnk','Cs','Echs1','Ivd',
+                    'Got2','Bcl2l1','Bcl2l2','Cyp27b1','Nrp1','Sfxn3','Timmdc1','Acsl1','Acsl4','Bak1','Bcl2','Cpt1c','Fundc1','Gdap1','Gk','Gpam',
+                    'Gpat2','Hk2','Letmd1','Maoa','Maob','Mavs','Mfn1','Mfn2','Miga1','Miga2','Mtch2','Mtx1','Pgam5','Rhot2','Rmdn3','Synj2bp',
+                    'Vdac2','Vps13a','Acsl6','Cpt1a','Hk1','Mtch1','Pink1','Bad','Sirt5','Polg','Bnip3'],
+
+
+
+    }
     
-    return clock_genes
+    return dict_list[type_]
